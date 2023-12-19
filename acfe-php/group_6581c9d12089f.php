b584a8b61efef9d1036a0fbef081aaf657238c41
Craig Allen <craig.c.allen@me.com>
2023-12-19 10:52:09 -0600

    Create group_6581c9d12089f.php

---
+++ acfe-php/group_6581c9d12089f.php
@@ -0,0 +1,59 @@
+<?php 
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_6581c9d12089f',
+	'title' => 'Local Test Sync',
+	'fields' => array(
+		array(
+			'key' => 'field_6581c9d1b00c2',
+			'label' => 'Local Test Sync Field 1',
+			'name' => 'local_test_sync_field_1',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'acfe_save_meta' => 0,
+			'default_value' => '',
+			'maxlength' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'left',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+	'acfe_autosync' => array(
+		0 => 'php',
+	),
+	'acfe_form' => 0,
+	'acfe_display_title' => '',
+	'acfe_meta' => '',
+	'acfe_note' => '',
+	'modified' => 1703004716,
+));
+
+endif;
