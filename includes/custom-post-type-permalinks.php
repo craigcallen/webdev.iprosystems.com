dd9280c9f7ba33eb045d6a56bbef02f5eb9835ea
Craig Allen <craig.c.allen@me.com>
2023-12-20 06:48:03 -0600

    Update custom-post-type-permalinks.php

--- includes/custom-post-type-permalinks.php
+++ includes/custom-post-type-permalinks.php
@@ -4,7 +4,7 @@ function custom_post_type_permalink($permalink, $post, $leavename) {
     $post_type = get_post_type($post);
 
     // Define the post types for which you want to change the permalink structure
-    $custom_post_types = array('weekly-ad', 'alert', 'promo', 'reward');
+    $custom_post_types = array('weekly-ad', 'alert', 'promo', 'reward', 'slider');
 
     // Check if the current post type is in the array
     if (in_array($post_type, $custom_post_types)) {
