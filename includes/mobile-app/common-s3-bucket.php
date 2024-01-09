ea953413a882a2cf75cc81c8f89e2dbacf412fc1
Craig Allen <craig.c.allen@me.com>
2024-01-09 07:28:29 -0600

    Update common-s3-bucket.php

--- includes/mobile-app/common-s3-bucket.php
+++ includes/mobile-app/common-s3-bucket.php
@@ -1,10 +1,11 @@
 <?
 function custom_as3cf_setting_bucket($bucket) {
-    // Set the same bucket for all subsites
-    $common_bucket = 'multisiteconfig';
+    // Set the bucket to a constant value 'multisiteconfig'
+    $bucket = 'multisiteconfig';
 
-    return $common_bucket;
+    // You can add additional logic here if needed
+
+    return $bucket;
 }
 
-// Hook the custom function to the as3cf_setting_bucket filter
 add_filter('as3cf_setting_bucket', 'custom_as3cf_setting_bucket');
