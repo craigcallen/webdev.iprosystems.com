dfdc345268256a5c12ea404439193aced34154b7
Craig Allen <craig.c.allen@me.com>
2024-01-10 16:12:06 -0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,8 +17,6 @@ if ($blog_id == 2) {
 }
 
 else {
-  
-}
 
 include( get_stylesheet_directory() . '/includes/404-redirect.php' );
 include( get_stylesheet_directory() . '/includes/acf-google-maps-api-key.php' );
@@ -50,3 +48,5 @@ include( get_stylesheet_directory() . '/includes/remove-modify-excerpt.php' );
 include( get_stylesheet_directory() . '/includes/remove-yoast-filters.php' );
 include( get_stylesheet_directory() . '/includes/site-title-shortcode.php' );
 include( get_stylesheet_directory() . '/includes/user-last-login.php' );
+
+}
